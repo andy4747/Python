e2ac0094a4883f324de160f77b3a8fda55002aa1
Angel Dhakal <51156237+AnjalDhakal@users.noreply.github.com>
2020-07-04 14:50:59 +0545

    stack DS

---
+++ lang/stack.py
@@ -0,0 +1,52 @@
+class Stack:
+    
+    def __init__(self):
+        self.items = []
+        
+    def push(self, item):
+        """
+        Push the elements at the end of the list
+        
+        :return: None
+        """
+        
+        self.items.append(item)
+        
+    def pop(self):
+        """
+        removes the item at the last index
+        
+        :return: None
+        """
+        self.items.pop()
+        
+        
+    def peek(self):
+        """
+        returns the item at the last index
+        
+        :return: list[-1]
+        """
+        if self.items:
+            return self.items[-1]
+        else:
+            return None
+    
+    def size(self):
+        """
+        returns the size of the stack
+        
+        :return: integer
+        """
+        return len(self.items)
+    
+    def is_empty(self):
+        """
+        checks if the list is empty or not
+        
+        :return: boolean
+        """
+        if self.items == []:
+            return True
+        else:
+            return False
