15b2f09eef954d460dbbe9923edfdb8378401285
Angel Dhakal <anjaldhakal01@gmail.com>
2020-10-28 18:08:48 +0545

    length property added to get the length of the linked list

--- DSA/linked_lists/linked_list.py
+++ DSA/linked_lists/linked_list.py
@@ -6,6 +6,9 @@ class LinkedList:
         self.head = Node()
     
     def append(self, data):
+        """
+        adds item to the end of the list
+        """
         new_node = Node(data)
         current_node = self.head
         while current_node.next != None:
@@ -13,11 +16,26 @@ class LinkedList:
         current_node.next = new_node
 
     def display(self):
+        """
+        prints all the items in linked list
+        """
         current_node = self.head
         while current_node.next != None:
             current_node = current_node.next
-            print(current_node.data)
+            print(current_node.data, end=" | ")
+        print()
 
+    @property
+    def length(self):
+        """
+        returns the length of the linked list
+        """
+        current_node = self.head
+        len = 0
+        while current_node.next != None:
+            current_node = current_node.next
+            len += 1
+        return len
 
 if __name__ == "__main__":
     l = LinkedList()
@@ -25,4 +43,5 @@ if __name__ == "__main__":
     l.append(20)
     l.append(30)
     l.append(50)
+    print(l.length)
     l.display()
